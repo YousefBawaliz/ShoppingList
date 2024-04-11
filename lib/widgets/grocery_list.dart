50a1d3b6b713da8c327e1f25fb4fcb7d7a5088c5
Bolz <yousef2k012@gmail.com>
2024-04-11 18:23:06 +0300

    tried FutureBuilder

--- lib/widgets/grocery_list.dart
+++ lib/widgets/grocery_list.dart
@@ -17,81 +17,63 @@ class GroceryList extends StatefulWidget {
 
 class _GroceryListState extends State<GroceryList> {
   List<GroceryItem> _groceryItems = [];
-  var _isLoading = true;
-  String? error;
 
-  //adding items using navigator.pop/push
-  // void _additem() async {
-  //   final _newItem = await Navigator.of(context).push<GroceryItem>(
-  //     MaterialPageRoute(
-  //       builder: (context) => NewItem(),
-  //     ),
-  //   );
-  //   if (_newItem == null) {
-  //     return;
-  //   }
-  //   setState(() {
-  //     _groceryItems.add(_newItem);
-  //   });
-  // }
+  late Future<List<GroceryItem>> _loadedItems;
+  String? error;
 
   @override
   void initState() {
-    _LoadItems();
+    _loadedItems = _LoadItems();
     super.initState();
   }
 
-  void _LoadItems() async {
+  Future<List<GroceryItem>> _LoadItems() async {
     final url = Uri.https(
         'flutter-prep-6fefd-default-rtdb.asia-southeast1.firebasedatabase.app',
         'shopping-list.json');
 
-    try {
-      final response = await http.get(url);
-      //statusCode 400/500 is erroe
-      if (response.statusCode > 400) {
-        setState(() {
-          error = 'Failed to fetch data  pls try again later';
-        });
-      }
-      print(response.body);
-
-      //to check if response is null (i.e no items in the backend)
-      if (response.body == 'null') {
-        setState(() {
-          _isLoading = false;
-        });
-        return;
-      }
-
-      final Map<String, dynamic> listData = jsonDecode(response.body);
-      final List<GroceryItem> loadedItems = [];
-      for (final item in listData.entries) {
-        //we're going through the entried in categories from categories.dart, and we're taking a look at those items,
-        //and for every item we check if the value(Category object) has a name that is equal to the value stored in
-        // the category key of the item that is part of the response:
-        final category = categories.entries
-            .firstWhere(
-                (element) => element.value.name == item.value['category'])
-            .value;
-        loadedItems.add(
-          GroceryItem(
-            category: category,
-            id: item.key,
-            name: item.value['name'],
-            quantity: item.value['quantity'],
-          ),
-        );
-      }
-      setState(
-        () {
-          _groceryItems = loadedItems;
-          _isLoading = false;
-        },
-      );
-    } catch (e) {
-      print("error");
+    // try {
+    final response = await http.get(url);
+    //statusCode 400/500 is erroe
+    if (response.statusCode > 400) {
+      throw Exception("failed to fetch gricery items");
     }
+    print(response.body);
+
+    //to check if response is null (i.e no items in the backend)
+    if (response.body == 'null') {
+      return [];
+    }
+
+    final Map<String, dynamic> listData = jsonDecode(response.body);
+    final List<GroceryItem> loadedItems = [];
+    for (final item in listData.entries) {
+      //we're going through the entried in categories from categories.dart, and we're taking a look at those items,
+      //and for every item we check if the value(Category object) has a name that is equal to the value stored in
+      // the category key of the item that is part of the response:
+      final category = categories.entries
+          .firstWhere((element) => element.value.name == item.value['category'])
+          .value;
+      loadedItems.add(
+        GroceryItem(
+          category: category,
+          id: item.key,
+          name: item.value['name'],
+          quantity: item.value['quantity'],
+        ),
+      );
+    }
+    return loadedItems;
+    // setState(
+    //   () {
+    //     _groceryItems = loadedItems;
+    //     _isLoading = false;
+    //   },
+    // );
+
+    // } catch (e) {
+    //   print("error");
+    // }
   }
 
   //adding items using database
@@ -139,41 +121,6 @@ class _GroceryListState extends State<GroceryList> {
 
   @override
   Widget build(BuildContext context) {
-    Widget content = const Center(
-      child: Text('No items yet'),
-    );
-
-    if (_isLoading) {
-      content = const Center(
-        child: CircularProgressIndicator(),
-      );
-    }
-
-    if (_groceryItems.isNotEmpty) {
-      content = ListView.builder(
-        itemCount: _groceryItems.length,
-        itemBuilder: (context, index) => Dismissible(
-          key: ValueKey(_groceryItems[index].id),
-          onDismissed: (direction) => _removeItem(_groceryItems[index]),
-          child: ListTile(
-            title: Text(_groceryItems[index].name),
-            leading: Container(
-              width: 24,
-              height: 24,
-              color: _groceryItems[index].category.color,
-            ),
-            trailing: Text(_groceryItems[index].quantity.toString()),
-          ),
-        ),
-      );
-    }
-
-    if (error != null) {
-      Widget content = Center(
-        child: Text(error!),
-      );
-    }
-
     return Scaffold(
         appBar: AppBar(
           actions: [
@@ -184,6 +131,45 @@ class _GroceryListState extends State<GroceryList> {
           ],
           title: Text("Your Groceries"),
         ),
-        body: content);
+        body: FutureBuilder(
+          future: _loadedItems,
+          builder: (context, snapshot) {
+            if (snapshot.connectionState == ConnectionState.waiting) {
+              return const Center(
+                child: CircularProgressIndicator(),
+              );
+            }
+
+            if (snapshot.hasError) {
+              return Center(
+                child: Text(snapshot.error
+                    .toString()), //gives us access to the error code at the top
+              );
+            }
+
+            if (snapshot.data!.isEmpty) {
+              return const Center(
+                child: Text('No items yet'),
+              );
+            }
+
+            return ListView.builder(
+              itemCount: snapshot.data!.length,
+              itemBuilder: (context, index) => Dismissible(
+                key: ValueKey(snapshot.data![index].id),
+                onDismissed: (direction) => _removeItem(_groceryItems[index]),
+                child: ListTile(
+                  title: Text(snapshot.data![index].name),
+                  leading: Container(
+                    width: 24,
+                    height: 24,
+                    color: snapshot.data![index].category.color,
+                  ),
+                  trailing: Text(snapshot.data![index].quantity.toString()),
+                ),
+              ),
+            );
+          },
+        ));
   }
 }
